023208ee6148d8a2ec8d1f7d421d75e38d916b6b
ViewCont <office@viewcont.com>
2015-08-05 21:08:53 +0300

    Променен текст на title атрибут

--- plugin/zoom-js/zoom.js
+++ plugin/zoom-js/zoom.js
@@ -179,9 +179,9 @@ var zoom = (function(){
 				if(c.search('chrome')!=-1){					
 
 					if(document.getElementsByClassName('bg-lang')[0].style.display=="none"){
-						event.target.title="Click to zoom out.";
+						event.target.title="If the image is blur, move out the cursor.";
 					} 
-					else {event.target.title="Кликнете за смаляване.";}
+					else {event.target.title="Ако изображението не е ясно, преместете курсора извън него.";}
 				}
 			}															//
 			else {														//
