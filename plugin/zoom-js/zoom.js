8758cdc0dea0c1d4c998ab996fe0f2b96fb0ad9d
ViewCont <office@viewcont.com>
2015-08-15 16:03:12 +0300

    test zoom

--- plugin/zoom-js/zoom.js
+++ plugin/zoom-js/zoom.js
@@ -197,7 +197,7 @@ var zoom = (function(){
         selection.removeAllRanges();
         selection.addRange(range);
     }	
-    document.getElementById('testdiv').click();					//
+//    document.getElementById('testdiv').click();					//
 //    console.log(document.getElementsByClassName('pronote')[0].id);
 				}																							//
 			}																								//
