46cec7ea9c0cd7bff2958d597b2bd33be5d7eb80
ViewCont <office@viewcont.com>
2015-08-16 16:18:05 +0300

    Hide address bar

--- js/reveal.js
+++ js/reveal.js
@@ -1300,7 +1300,7 @@
 		if( config.hideAddressBar && isMobileDevice ) {
 			// Events that should trigger the address bar to hide
 			window.addEventListener( 'load', removeAddressBar, false );
-			window.addEventListener( 'orientationchange', removeAddressBar, false );
+//			window.addEventListener( 'orientationchange', removeAddressBar, false ); // Added by Peter Petrov
 		}
 
 	}
