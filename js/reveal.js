d37864cb340b7dd1de2ea6bf8ff5cb3295a9887d
ViewCont <office@viewcont.com>
2015-08-16 18:09:47 +0300

    test

--- js/reveal.js
+++ js/reveal.js
@@ -4129,11 +4129,11 @@ case 40: navigateDown(); break; //
 	      case 90:
 	        document.querySelectorAll(".reveal section.present a.navigate-down.arrow.enabled")[0].style.display = "none";
 	        alert(document.querySelectorAll(".reveal section.present center.quest")[0].style.width);
-	        document.querySelectorAll(".reveal section.present center.quest")[0].style.textAlign = "right";
+//	        document.querySelectorAll(".reveal section.present center.quest")[0].style.textAlign = "right";
 	        break; 
 	      default:
 	        document.querySelectorAll(".reveal section.present a.navigate-down.arrow.enabled")[0].style.display = "inline";
-	        document.querySelectorAll(".reveal section.present center.quest")[0].style.textAlign = "center";
+//	        document.querySelectorAll(".reveal section.present center.quest")[0].style.textAlign = "center";
 	        break; 
 	    }
 						// 
