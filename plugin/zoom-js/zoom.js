e6fc52dcdf0559041a566ed5ce56d9e723601fae
ViewCont <office@viewcont.com>
2015-08-20 19:24:31 +0300

    test

--- plugin/zoom-js/zoom.js
+++ plugin/zoom-js/zoom.js
@@ -16,8 +16,8 @@ var zoomflag = 0; // Added by Peter Petrov
 			var modifier = event.target.className; 												// Changed by Peter Petrov
 			var zoomPadding = 25; 																// Changed from 20 to 25 by Peter Petrov
 			var revealScale = Reveal.getScale();												//
-			if( modifier == "mozoom" || modifier == "zoomel mozoom") { 							//
-			event.preventDefault();																//	
+			event.preventDefault();																//
+			if( modifier == "mozoom" || modifier == "zoomel mozoom") { 							//	
 				if(c.search('chrome')==-1){	zoom.to({element: event.target, pan: false});} 		// Added by Peter Petrov
 				else{ 																			//
 					var bounds = event.target.getBoundingClientRect();							//
@@ -31,12 +31,12 @@ var zoomflag = 0; // Added by Peter Petrov
 						});																		//
 					}																			//	Added by Peter Petrov
 				} 																				//
-	    } else {																				//
-	        timeout = setTimeout(function() {													//
-	            clearTimeout(timeout);															//
-	        }, 500);																			//
-	    }																						//
-	    lastTap = currentTime;																	//
+		    } else {																				//
+		        timeout = setTimeout(function() {													//
+		            clearTimeout(timeout);															//
+		        }, 500);																			//
+		    }																						//
+		    lastTap = currentTime;																	//
 	});																							//
 	if(c.search('mobile')==-1){
 		document.querySelector( '.reveal .slides' ).addEventListener( 'click', function( event ) { 
