57c0009eb9c9800eb4a10d50b6f9b4db45c9a370
ViewCont <office@viewcont.com>
2015-08-20 18:25:28 +0300

    test

--- plugin/zoom-js/zoom.js
+++ plugin/zoom-js/zoom.js
@@ -16,11 +16,10 @@ document.querySelector( '.reveal .slides' ).addEventListener('touchend', functio
 		var modifier = event.target.className; // Changed by Peter Petrov
 		var zoomPadding = 25; // Changed from 20 to 25 by Peter Petrov
 		var revealScale = Reveal.getScale();
-		if( modifier == "mozoom" ) { 													// 
-			if(c.search('chrome')==-1){			alert(c); 	event.preventDefault();	zoom.to({element: event.target, pan: false});} 		// Added by Peter Petrov
-			else{ 
-																//
-				event.preventDefault();
+		if( modifier == "mozoom" ) { 
+		event.preventDefault();														// 
+			if(c.search('chrome')==-1){	zoom.to({element: event.target, pan: false});} 		// Added by Peter Petrov
+			else{ 										//
 				var bounds = event.target.getBoundingClientRect();
 				var u = navigator.userAgent, ua = u.toLowerCase();
 				zoom.to({
@@ -32,11 +31,8 @@ document.querySelector( '.reveal .slides' ).addEventListener('touchend', functio
 					});	
 				}																//	Added by Peter Petrov
 			} 
-//        event.preventDefault();
     } else {
- //       alert('Single Tap');
         timeout = setTimeout(function() {
- //           alert('Single Tap (timeout)');
             clearTimeout(timeout);
         }, 500);
     }
@@ -49,14 +45,12 @@ document.querySelector( '.reveal .slides' ).addEventListener('touchend', functio
 		var zoomPadding = 25; // Changed from 20 to 25 by Peter Petrov
 		var revealScale = Reveal.getScale();
 
-		if( modifier == "zoomel" ) { 														// 
-			if(c.search('chrome')==-1){event.preventDefault(); zoom.to({element: event.target, pan: false});} 		// Added by Peter Petrov
+		if( modifier == "zoomel" ) { 
+			event.preventDefault(); 														// 
+			if(c.search('chrome')==-1){zoom.to({element: event.target, pan: false});} 		// Added by Peter Petrov
 			else{ 																			//
-				event.preventDefault();
-				
 				var bounds = event.target.getBoundingClientRect();
 				var u = navigator.userAgent, ua = u.toLowerCase();
-				
 				zoom.to({
 					x: ( bounds.left * revealScale ) - zoomPadding,
 					y: ( bounds.top * revealScale ) - zoomPadding,
