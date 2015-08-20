91927fa9f28389c337c00559d2745ac96819e160
ViewCont <office@viewcont.com>
2015-08-20 14:35:11 +0300

    test

--- plugin/zoom-js/zoom.js
+++ plugin/zoom-js/zoom.js
@@ -5,7 +5,27 @@ var zoomflag = 0; // Added by Peter Petrov
 
 	function css_browser_selector(u){var ua=u.toLowerCase(),is=function(t){return ua.indexOf(t)>-1},g='gecko',w='webkit',s='safari',o='opera',m='mobile',h=document.documentElement,b=[(!(/opera|webtv/i.test(ua))&&/msie\s(\d)/.test(ua))?('ie ie'+RegExp.$1):is('firefox/2')?g+' ff2':is('firefox/3.5')?g+' ff3 ff3_5':is('firefox/3.6')?g+' ff3 ff3_6':is('firefox/3')?g+' ff3':is('gecko/')?g:is('opera')?o+(/version\/(\d+)/.test(ua)?' '+o+RegExp.$1:(/opera(\s|\/)(\d+)/.test(ua)?' '+o+RegExp.$2:'')):is('konqueror')?'konqueror':is('blackberry')?m+' blackberry':is('android')?m+' android':is('chrome')?w+' chrome':is('iron')?w+' iron':is('applewebkit/')?w+' '+s+(/version\/(\d+)/.test(ua)?' '+s+RegExp.$1:''):is('mozilla/')?g:'',is('j2me')?m+' j2me':is('iphone')?m+' iphone':is('ipod')?m+' ipod':is('ipad')?m+' ipad':is('mac')?'mac':is('darwin')?'mac':is('webtv')?'webtv':is('win')?'win'+(is('windows nt 6.0')?' vista':''):is('freebsd')?'freebsd':(is('x11')||is('linux'))?'linux':'','js']; c = b.join(' '); h.className += ' '+c; return c;}; 
 	css_browser_selector(navigator.userAgent);
-	document.querySelector( '.reveal .slides' ).addEventListener( 'dblclick', function( event ) { 
+	var clickTimer = null;
+	document.querySelector( '.reveal .slides' ).addEventListener( 'click', function( event ) { 
+
+
+
+
+    if (clickTimer == null) {
+        clickTimer = setTimeout(function () {
+            clickTimer = null;
+            alert("single");
+
+        }, 500)
+    } else {
+        clearTimeout(clickTimer);
+        clickTimer = null;
+ //       alert("double");
+
+
+
+
+
 	
 		var modifier = event.target.className; // Changed by Peter Petrov
 		
@@ -29,6 +49,8 @@ var zoomflag = 0; // Added by Peter Petrov
 					});	
 				}																//	Added by Peter Petrov
 			} 
+    } //
+
 	} );
 
 	Reveal.addEventListener( 'overviewshown', function() { isEnabled = false; } );
